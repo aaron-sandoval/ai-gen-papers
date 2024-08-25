6d5441f993f53aa08c81e2bc8b7059e63b9e1399
layla <layla.adam.work@gmail.com>
2024-08-25 23:03:48 +0200

    displayPDF

--- Front-End/simpleapp.py
+++ Front-End/simpleapp.py
@@ -87,3 +87,14 @@ if not check_password():
 # Main Streamlit app starts here
 st.write("Here goes your normal Streamlit app...")
 st.button("Click me")
+
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
