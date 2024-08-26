e059546b2cb4d18c886f8ab6518b83357250aaed
aaron-sandoval <32021231+aaron-sandoval@users.noreply.github.com>
2024-08-25 20:18:22 -0600

    another workaround to pdf display

--- Front_End/simpleapp.py
+++ Front_End/simpleapp.py
@@ -75,7 +75,12 @@ def main():
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
         # Embedding PDF in HTML
-        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+        pdf_display =  f"""<embed
+        class="pdfobject"
+        type="application/pdf"
+        title="Embedded PDF"
+        src="data:application/pdf;base64,{base64_pdf}"
+        style="overflow: auto; width: 100%; height: 300%;">"""
 
         # Displaying File
         st.markdown(pdf_display, unsafe_allow_html=True)
